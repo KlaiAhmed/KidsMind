dd180e081a74b6e01f23289bd6320d439e7591f9
Ahmed Klai <ahmedklai.connect@gmail.com>
2026-03-07 12:07:43 +0100

    chore: Added HF_TOKEN for faster faster-whisper model download

--- Services/stt/app/core/config.py
+++ Services/stt/app/core/config.py
@@ -4,7 +4,6 @@ SERVICE_NAME = "stt-service"
 
 WHISPER_MODE= getenv("WHISPER_MODE", "gpu")  # "cpu" or "gpu"
 
-
 if WHISPER_MODE == "gpu":
     # GPU settings
     WHISPER_DEVICE = "cuda"
@@ -13,10 +12,10 @@ if WHISPER_MODE == "gpu":
 else:
     # CPU settings
     WHISPER_DEVICE = "cpu"
-    WHISPER_COMPUTE_TYPE =  "int8" # "int8" is faster on CPU but slightly less accurate than "float32". Adjust as needed.
+    WHISPER_COMPUTE_TYPE =  "int8" # "int8" is faster on CPU but slightly less accurate than "int8_float32", "float32". Adjust as needed.
     WHISPER_CPU_THREADS = int(getenv("WHISPER_CPU_THREADS", "8"))
 
-WHISPER_MODEL = getenv("WHISPER_MODEL", "medium")
-WHISPER_NUM_WORKERS = int(getenv("WHISPER_NUM_WORKERS", "1"))
+WHISPER_MODEL = getenv("WHISPER_MODEL", "large-v3-turbo")
+WHISPER_NUM_WORKERS = int(getenv("WHISPER_NUM_WORKERS", "2"))
 
 print(f"Configuration : MODE={WHISPER_MODE}, MODEL={WHISPER_MODEL}, DEVICE={WHISPER_DEVICE}, COMPUTE_TYPE={WHISPER_COMPUTE_TYPE}" + (f", CPU_THREADS={WHISPER_CPU_THREADS}" if WHISPER_MODE == "cpu" else "") + f", NUM_WORKERS={WHISPER_NUM_WORKERS}")
